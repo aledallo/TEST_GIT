f1c43fe0a9ce812856b17cb59705ce9232119588
aledallo <alessandrodalloca5@gmail.com>
2020-11-08 19:27:42 +0100

    second commit

--- GIT_lezione1.py
+++ GIT_lezione1.py
@@ -1,5 +1,11 @@
 #questi sistemi di versioning system permette di salvare regolrmente lo stato di questo codice
 #in questo modo se voglio posso tornare indietro
 
+#ROSSO = IL FILE NON E' IN COMMIT
+#VERDE = IL FILE CINCIDE CON L'ULTIMA VERSIONE SALVATA
+#AZZURRO = IL FILE E' CAMBIATO E NON E' STATO SALVATO SUL PC
+
 
 print('ciao')
+print('ale')
+print('dallo')
